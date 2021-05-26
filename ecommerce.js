8c0c206a98d69bb839bcec5fe5510758bd9ac45b
mdshahnawazz <faizmerchant65@gmail.com>
2021-05-27 01:13:46 +0530

    shopping-site

---
+++ ecommerce.js
@@ -0,0 +1,8 @@
+var timer = new Timer();
+	timer.start({countdown: true, startValues: {seconds: 1000}});
+
+		document.querySelector('.values').innerHTML = timer.getTimeValues().toString();
+
+	timer.addEventListener('secondsUpdated', function (e) {
+    	document.querySelector('.values').innerHTML = timer.getTimeValues().toString();
+	});
